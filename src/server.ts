d4f17f33b624cb43d9a82d490219a763ec829d09
shahzaibahmadpawar1 <shazi8402@gmail.com>
2026-04-04 14:30:28 +0500

    resolved cors error

--- src/server.ts
+++ src/server.ts
@@ -30,11 +30,25 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+const allowedOrigins = [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'https://stg.pms.darbstations.com.sa',
+    'https://pms.darbstations.com.sa',
+    process.env.CORS_ORIGIN
+].filter(Boolean) as string[];
+
 app.use(cors({
-    origin: "https://stg.pms.darbstations.com.sa",
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
-    credentials: true
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.options('*', cors());
 app.use(express.json());
